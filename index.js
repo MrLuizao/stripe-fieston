fba10c289ba139a1ca124df310604423ad3a5a44
Luis Vazquez <luison@iMac-de-Luis.local>
2022-08-04 14:50:12 -0500

    add environments

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.post('/stripe-checkout', async (req, res) => {
 
 });
 
-app.listen(3000, ()=>{
-    console.log('Server PORT 3000!!!');
+let port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log('Server runing on =>', port);
 })
