c166e6c31d072d665e4f22c3502a936a7c849b9c
Luis Vazquez <luison@iMac-de-Luis.local>
2022-08-04 19:53:10 -0500

    uodate sms

--- twilio.js
+++ twilio.js
@@ -1,4 +1,4 @@
-const twilio = require ('twilio');
+const twilio = require('twilio');
 
 const accountSid = 'AC2715a13a44593c4c83c9f904721ccb51';
 const authToken = '58e24ffdeac632d15832d296b7d626f2';
