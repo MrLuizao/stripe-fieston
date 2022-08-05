3a73f1904182f9fce583db69fd23e76330737b7f
Luis Vazquez <luison@iMac-de-Luis.local>
2022-08-04 23:41:39 -0500

    chenge index to server name

--- sms.js
+++ sms.js
@@ -1,5 +1,7 @@
 const twilio = require('twilio');
-const process = require('../fieston-api/process')
+// const process = require('../fieston-api/process')
+const process = require('./process')
+
 
 // const accountSid = 'AC2715a13a44593c4c83c9f904721ccb51';
 // const authToken = '58e24ffdeac632d15832d296b7d626f2';
