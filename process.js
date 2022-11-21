9862f30cf798205ae8dc3abf23c562454e37a4d5
Luis Vazquez <luisvazquez@MacBook-de-Luis.local>
2022-11-21 11:13:38 -0600

    update port to deploy in railway

--- process.js
+++ process.js
@@ -1,7 +1,7 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 5000,
 
     SID: 'AC2715a13a44593c4c83c9f904721ccb51',
     AUTH: '58e24ffdeac632d15832d296b7d626f2'
