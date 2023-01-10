09327b1d71e7153e7967fc78689e0a3d6754f6bd
Luis Vazquez <lvazquez@ari.group>
2023-01-09 18:08:07 -0600

    update twilio credetials

--- process.js
+++ process.js
@@ -3,7 +3,10 @@ module.exports = {
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 5000,
 
-    SID: 'AC2715a13a44593c4c83c9f904721ccb51',
-    AUTH: '58e24ffdeac632d15832d296b7d626f2'
+    // SID: 'AC2715a13a44593c4c83c9f904721ccb51',
+    // AUTH: '58e24ffdeac632d15832d296b7d626f2'
+
+    SID: 'ACcf77cf54cfa9127ca7192ce140b301d7',
+    AUTH: '3e4ef091ebc789454fbb2a9eb12261cc'
 }
 
--- sms.js
+++ sms.js
@@ -10,7 +10,9 @@ const createSMS = () => {
     client.messages.create( {
         body: '¡Contrataron tus servicios! Revisa tu app FiestOn para conocer los detalles',
         to: '+525513666143',
-        from: '+19702933035'
+        // from: '+19702933035'
+        from: '+17262042552'
+        
     }).then( (message)=> console.log(message.sid));
 }
 
